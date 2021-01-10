9933139e542f645f04f2cb983630b7529b2de94f
njs04210 <pearlnkim@gmail.com>
2021-01-10 22:22:50 +0900

    [Design-Pattern] State 정리

---
+++ Design-Pattern/State/src/ModeSwtich.java
@@ -0,0 +1,11 @@
+public class ModeSwtich {
+    private ModeState modeState = new ModeStateLight(); //modeState가 Light모드로 초기화
+
+    public void setState(ModeState modeState) { //modeState 정하는 함수
+        this.modeState = modeState;
+    }
+
+    public void onSwitch() {
+        modeState.toggle(this);
+    }
+}
---
+++ Design-Pattern/State/src/MyProgram.java
@@ -0,0 +1,10 @@
+public class MyProgram {
+    public static void main(String[] args) {
+        ModeSwtich modeSwtich = new ModeSwtich();
+
+        modeSwtich.onSwitch();
+        modeSwtich.onSwitch();
+        modeSwtich.onSwitch();
+        modeSwtich.onSwitch();
+    }
+}
